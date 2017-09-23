b08326cc166d55406535dd203df193ecce817d54
varma2fork <dvarma97@gmail.com>
2017-09-23 13:12:41 -0700

    Enable APP to yse Giphy Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome to GiphySearch!';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  giphies = [];
+
+  constructor(http: Http) { this.http = http; }
+
+  performSearch(nik) {
+    var apiLink = this.link + nik.value; this.http.request(apiLink) .subscribe((res: Response) => {  this.giphies = res.json().data; });
+  }
 }
